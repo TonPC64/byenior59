072423400922c35ff45b3a62877ae944ed593736
kingkong64 <kingkong2103@gmail.com>
2016-04-23 02:23:25 +0700

    initDB

--- public/js/slot.js
+++ public/js/slot.js
@@ -2,26 +2,32 @@
 /* global angular,$ */
 angular.module('slots', []).directive('slot', function () {
   var der = {
+    time: '',
+    result: '',
     restrict: 'E',
     template: '<div class="slots" id="slots"><div class="wrapper"></div></div>',
     fn: {
       opts: [],
       go: function (elem) {
+        der.fn.clearSlots(elem)
         der.fn.addSlots(elem)
         der.fn.moveSlots(elem)
       },
+      clearSlots: function (elem) {
+        elem.children().remove()
+        elem.css('margin-top', 7)
+      },
       addSlots: function (elem) {
-        for (var i = 0; i < 15; i++) {
-          var ctr = Math.floor(Math.random() * der.fn.opts.length)
-          elem.append("<div class='slot'>" + der.fn.opts[ctr] + '</div>')
-        }
+        der.fn.opts.forEach((item) => {
+          elem.append("<div class='slot'>" + item + '</div>')
+        })
       },
       moveSlots: function (elem) {
-        var marginTop = parseInt(elem.css('margin-top'), 10)
-        marginTop -= (7 * 100)
-
-        var time = 6500
-        time += Math.round(Math.random() * 1000)
+        var marginTop = parseInt(elem.css('margin-top'), 0)
+        marginTop -= ((parseInt(der.time, 0)) * 100)
+        var time = der.time * 1000
+        der.result = der.fn.opts[der.time]
+        console.log(der.fn.opts[der.time])
         elem.stop(true, true)
 
         elem.animate(
@@ -35,30 +41,64 @@ angular.module('slots', []).directive('slot', function () {
           scope.$watch(attrs.trigger, function (newValue, oldValue) {
             if (newValue) {
               der.fn.opts = scope[attrs.dataslot]
+              der.time = attrs.time
               der.fn.go($('#' + attrs.id + ' #slots .wrapper'))
+              if (attrs.func != null) scope[attrs.func](der.result)
             }
           })
         }
       }
-      console.log(scope[attrs.dataslot])
     }
   }
   return der
 })
 
 var myApp = angular.module('myApp', ['slots'])
-myApp.controller('MyCtrl', function ($scope, $http) {
-  $http.get('/register').success(function (req, res) {
-    $scope.dataname = req.map((item) => item.name)
-    $scope.datasid = req.map((item) => item.sid.substr(0, 2))
-    $scope.datapre = req.map((item) => item.pre)
-  })
+myApp.controller('MyCtrl', function ($scope, $http, $timeout) {
+  var reciveData = function () {
+    $http.get('/register').success(function (req, res) {
+      $scope.rand = Math.round(Math.random() * (req.map((item) => item.name).length) * 3)
+      $scope.dataname = setData(req.map((item) => item.name), 30)
+      $scope.datasid = setData(req.map((item) => 'ปี ' + item.sid.substr(0, 2)), 10)
+      $scope.datapre = setData(req.map((item) => item.pre), 15)
+    // updateData($scope.dataname[34])
+    })
+  }
+
+  reciveData()
   $scope.myTrigger = false
-  $scope.myTrigger1 = false
-  $scope.myTrigger2 = false
   $scope.btnClick = function () {
+    reciveData()
     $scope.myTrigger = !$scope.myTrigger
-    $scope.myTrigger1 = !$scope.myTrigger
-    $scope.myTrigger2 = !$scope.myTrigger
+    $timeout(function () { $scope.myTrigger = false }, 30000)
+  }
+
+  $scope.updateData = function (name) {
+    $http.put('/register', {name: name}).success(function (req, res) {})
+  }
+
+  var setData = function (arr, len) {
+    var data = arr
+    data.forEach((item) => {
+      arr.push(item)
+    })
+    data.forEach((item) => {
+      arr.push(item)
+    })
+    var temp = arr.splice(arr.length - len, len)
+    temp = temp.reverse()
+    temp.forEach((item) => {
+      arr.splice(0, 0, item)
+    })
+    return shuffleData(arr)
+  }
+
+  var shuffleData = function (arr) {
+    var temp = arr.splice(arr.length - $scope.rand, $scope.rand)
+    temp = temp.reverse()
+    temp.forEach((item) => {
+      arr.splice(0, 0, item)
+    })
+    return arr
   }
 })
--- server.js
+++ server.js
@@ -6,14 +6,28 @@ var app = express()
 mongoose.connect('mongodb://localhost/byenior')
 var Schema = mongoose.Schema
 var thingSchema = new Schema({}, { strict: false })
-var users = mongoose.model('users', thingSchema)
+var Users = mongoose.model('users', thingSchema)
 var Registers = mongoose.model('registers', thingSchema)
 
 app.set('port', (process.env.PORT || 80))
 app.use(express.static('public'))
 
+app.get('/users', function (req, res) {
+  Users.find(function (err, data) {
+    if (err === null) res.send(data)
+    else res.sendStatus(400)
+  })
+})
+
 app.get('/register', function (req, res) {
-  Registers.find(function (err, data) {
+  Registers.find({permission: true}, function (err, data) {
+    if (err === null) res.send(data)
+    else res.sendStatus(400)
+  })
+})
+
+app.put('/register', jsonParser, function (req, res) {
+  Registers.update({'name': req.body.name}, {$set: {'permission': false}}, function (err, data) {
     if (err === null) res.send(data)
     else res.sendStatus(400)
   })
@@ -29,7 +43,7 @@ app.post('/checkRegis', jsonParser, function (req, res) {
 })
 
 app.post('/register', jsonParser, function (req, res) {
-  users.find({ sid: req.body.data }, function (err, data) {
+  Users.find({ sid: req.body.data }, function (err, data) {
     if (err === null) res.send(data)
     else res.sendStatus(400)
   })
@@ -41,13 +55,17 @@ app.post('/insert', jsonParser, function (req, res) {
   res.send(insert)
 })
 
-// var fs = require('fs');
-// var obj = JSON.parse(fs.readFileSync('public/resource/data.json', 'utf8'))
-// obj.data.forEach( (item) => {
-//   var insert = new users(item)
-//   insert.save()
-// })
+var initDB = function () {
+  Users.remove({})
+  var fs = require('fs')
+  var obj = JSON.parse(fs.readFileSync('public/resource/data.json', 'utf8'))
+  obj.data.forEach((item) => {
+    var insert = new Users(item)
+    insert.save()
+  })
+}
 
+// initDB()
 app.listen(app.get('port'), function () {
   console.log('Server Start at port ', app.get('port'))
 })
